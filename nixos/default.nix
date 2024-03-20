492f91cb94f95f32917d3d43c4d77f4160b8f4e8
Jiucheng <zangjiucheng@gmail.com>
2024-03-20 15:15:04 -0400

    delete nixos

--- nixos/default.nix
+++
@@ -1,9 +0,0 @@
-{
-  imports =
-    [ # Include the results of the hardware scan.
-      ./user/jiucheng/home.nix
-      #./user/default/home.nix
-      ./desktopenv.nix
-      ./samba.nix
-    ];
-}

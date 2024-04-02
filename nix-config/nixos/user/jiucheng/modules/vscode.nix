b05394c96eecefd5899c26ab31a42fcf285095a0
Jiucheng Zang <zangjiucheng@gmail.com>
2024-04-02 10:34:24 -0400

    update + fix nixos-dev branch

---
+++ nix-config/nixos/user/jiucheng/modules/vscode.nix
@@ -0,0 +1,10 @@
+{ pkgs, ... }:
+{
+  programs.vscode = {
+    enable = true;
+    extensions = with pkgs.vscode-extensions; [
+      vscodevim.vim
+      github.github-vscode-theme
+    ];
+  };
+}

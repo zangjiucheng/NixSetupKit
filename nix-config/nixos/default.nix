670a6c17451b6794ea7f50e965efb928508ff528
Jiucheng Zang <zangjiucheng@gmail.com>
2024-04-12 14:57:03 -0400

    update

---
+++ nix-config/nixos/default.nix
@@ -0,0 +1,10 @@
+{
+  imports =
+    [
+      # Include the results of the hardware scan.
+      ../user/jiucheng/home.nix
+      #./user/default/home.nix
+      ./desktopenv.nix
+      ./samba.nix
+    ];
+}

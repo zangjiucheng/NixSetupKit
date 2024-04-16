9f26cfa3c94c79782b30b72b4bda7141301f8dfe
zangjiucheng <zangjiucheng@gmail.com>
2024-04-16 16:04:23 -0400

    update dir setup

--- nix-config/user/jiucheng/home.nix
+++ nix-config/user/jiucheng/home.nix
@@ -74,7 +74,7 @@
 
       ".config/htop/htoprc".source = dotfiles/htoprc;
       ".config/qterminal.org/qterminal.ini".source = dotfiles/qterminal;
-      ".background-image".source = background-image/fire;
+      ".background-image".source = background-image/cityGirl;
       # # You can also set the file content immediately.
       # ".gradle/gradle.properties".text = ''
       #   org.gradle.console=verbose

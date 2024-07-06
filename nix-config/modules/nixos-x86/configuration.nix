7efea41483c3309ee0555db7934715dff6cf93a0
Jiucheng Zang <zangjiucheng@gmail.com>
2024-07-06 18:00:41 -0400

    Display vsync and external setup

--- nix-config/modules/nixos-x86/configuration.nix
+++ nix-config/modules/nixos-x86/configuration.nix
@@ -4,38 +4,42 @@
 
 { config, pkgs, home-manager, ... }:
 {
-  imports =
-    [
+  imports = [
       ../shared/configuration.nix
       ../../user/user-nixos/userList.nix
       ./zfs.nix
-    ];
+  ];
 
-    # x86-nixos Software
-    environment.systemPackages = pkgs.callPackage ./package.nix {};
+  # x86-nixos Software
+  environment.systemPackages = pkgs.callPackage ./package.nix {};
 
-    # Use Latest Support kernelPackages for ZFS
-    boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
+  # Use Latest Support kernelPackages for ZFS
+  boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
 
-    services.tlp = {
-          enable = true;
-          settings = {
-            CPU_SCALING_GOVERNOR_ON_AC = "performance";
-            CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+  services.tlp = {
+        enable = true;
+        settings = {
+          CPU_SCALING_GOVERNOR_ON_AC = "performance";
+          CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
-            CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-            CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+          CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+          CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
 
-            CPU_MIN_PERF_ON_AC = 0;
-            CPU_MAX_PERF_ON_AC = 100;
-            CPU_MIN_PERF_ON_BAT = 0;
-            CPU_MAX_PERF_ON_BAT = 70;
+          CPU_MIN_PERF_ON_AC = 0;
+          CPU_MAX_PERF_ON_AC = 100;
+          CPU_MIN_PERF_ON_BAT = 0;
+          CPU_MAX_PERF_ON_BAT = 70;
 
-           #Optional helps save long term battery health
-            START_CHARGE_THRESH_BAT0 = 80; # 40 and bellow it starts to charge
-            STOP_CHARGE_THRESH_BAT0 = 95; # 80 and above it stops charging
+         #Optional helps save long term battery health
+          START_CHARGE_THRESH_BAT0 = 80; # 40 and bellow it starts to charge
+          STOP_CHARGE_THRESH_BAT0 = 95; # 80 and above it stops charging
 
-          };
-    };
+        };
+  };
+
+  services.picom = {
+    enable = true;
+    vSync = true;
+  };
 
 }

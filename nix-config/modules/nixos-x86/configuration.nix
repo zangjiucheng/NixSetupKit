1692b9a2f5bd9bfcc31055249a6874c42dfaa0ea
Jiucheng(Oliver) <56462380+zangjiucheng@users.noreply.github.com>
2024-07-10 17:09:24 -0400

    Dev-NixOS Update (#14)
    * Update Some System Base Setup ZSH
    
    * Update zfs setup
    
    * Display vsync and external setup
    
    * Update oh-my-zsh Setup
    
    * Update ThinkPad Setup For nix-config

--- nix-config/modules/nixos-x86/configuration.nix
+++ nix-config/modules/nixos-x86/configuration.nix
@@ -40,7 +40,15 @@
 
         };
   };
-
+  
+  services.samba-wsdd = {
+    enable = true;
+    openFirewall = true;
+  };
+  
+  networking.firewall.enable = true;
+  networking.firewall.allowPing = true;
+  
   services.picom = {
     enable = true;
     vSync = true;

aae70faa1be3351fd300e5eba404f26b67a9e1fc
Jiucheng(Oliver) <56462380+zangjiucheng@users.noreply.github.com>
2024-07-10 17:09:24 -0400

    Dev-NixOS Update (#14)
    * Update Some System Base Setup ZSH
    
    * Update zfs setup
    
    * Display vsync and external setup
    
    * Update oh-my-zsh Setup
    
    * Update ThinkPad Setup For nix-config

--- nix-config/modules/nixos-x86/zfs.nix
+++ nix-config/modules/nixos-x86/zfs.nix
@@ -1,6 +1,7 @@
 {
   boot.supportedFilesystems = [ "zfs" ];
   boot.zfs.forceImportRoot = false;
-  # boot.zfs.extraPools = ["datapool"];
+  boot.zfs.extraPools = ["datapool"];
+
   networking.hostId = "ac1ffa95"; # generate by head -c4 /dev/urandom | od -A none -t x4
 }

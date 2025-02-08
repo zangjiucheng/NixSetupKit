56147aabe0d8306f2bb9c46ab1b18b6b0af370fd
Jiucheng Zang <git.jiucheng@gmail.com>
2025-02-08 13:28:59 -0500

    Update NixOS Config

--- nix-config/user/user-nixos/jiucheng/home.nix
+++ nix-config/user/user-nixos/jiucheng/home.nix
@@ -183,9 +183,29 @@ in
       enable = true;
       associations.added = {
         "inode/directory" = [ "org.gnome.Nautilus.desktop" ];
+        "x-scheme-handler/http" = [ "zen.desktop" ];
+        "x-scheme-handler/https" = [ "zen.desktop" ];
+        "x-scheme-handler/chrome" = [ "zen.desktop" ];
+        "text/html" = [ "zen.desktop" ];
+        "application/x-extension-htm" = [ "zen.desktop" ];
+        "application/x-extension-html" = [ "zen.desktop" ];
+        "application/x-extension-shtml" = [ "zen.desktop" ];
+        "application/xhtml+xml" = [ "zen.desktop" ];
+        "application/x-extension-xhtml" = [ "zen.desktop" ];
+        "application/x-extension-xht" = [ "zen.desktop" ];
       };
       defaultApplications = {
         "inode/directory" = [ "org.gnome.Nautilus.desktop" ];
+        "x-scheme-handler/http" = [ "zen.desktop" ];
+        "x-scheme-handler/https" = [ "zen.desktop" ];
+        "x-scheme-handler/chrome" = [ "zen.desktop" ];
+        "text/html" = [ "zen.desktop" ];
+        "application/x-extension-htm" = [ "zen.desktop" ];
+        "application/x-extension-html" = [ "zen.desktop" ];
+        "application/x-extension-shtml" = [ "zen.desktop" ];
+        "application/xhtml+xml" = [ "zen.desktop" ];
+        "application/x-extension-xhtml" = [ "zen.desktop" ];
+        "application/x-extension-xht" = [ "zen.desktop" ];
       };
     };
 

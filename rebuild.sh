50069b6986a8e4ecf65ae70455a576713bb9d0fb
Jiucheng Zang <git.jiucheng@gmail.com>
2025-02-09 14:33:47 -0500

    Initial rebuild.sh inspired from twl config

---
+++ rebuild.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+set -eu
+
+NIXOSCONFIG="${1:-nixos}"
+GITBRANCH="${2:-build}"
+
+git switch --quiet --merge "$GITBRANCH"
+
+if ! git diff --quiet --no-ext-diff; then
+  git commit --quiet --all --message '_'
+fi
+
+if git diff --quiet --no-ext-diff main; then
+  exit 0
+fi
+
+rm -f result
+
+if ! sudo nixos-rebuild switch --show-trace \
+  --flake ".#${NIXOSCONFIG}" \
+  >/dev/null; then
+  exit 1
+fi
+
+CURRENT="$(nixos-rebuild list-generations | grep -m 1 current)"
+git switch main
+git merge --squash "$GITBRANCH"
+git commit --message "$CURRENT"
+git branch -f "$GITBRANCH"

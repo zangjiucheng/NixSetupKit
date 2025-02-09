96279d3d6e1fd9a7bb3221d66b126484a7d64f33
Jiucheng Zang <git.jiucheng@gmail.com>
2025-02-09 15:17:05 -0500

    343 current  2025-02-09 14:31:10  x86-user-24.11.20250206.f5a32fa  6.6.75                          *

--- user/user-nixos/jiucheng/home.nix
+++ user/user-nixos/jiucheng/home.nix
@@ -2,13 +2,14 @@
 let 
   share_folder = "../../user-share";
   homeDir = builtins.getEnv "HOME";
+  user = "jiucheng";
 in
-{
+  {
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  
+
   programs.zsh.enable = true;
 
-  users.users.jiucheng = {
+  users.users.${user} = {
     isNormalUser = true;
     extraGroups = [ "i2c" "wheel" ];
     shell = pkgs.zsh;
@@ -20,11 +21,11 @@ in
 
   # home-manager.backupFileExtension = "backup";
 
-  home-manager.users.jiucheng = { config, pkgs, ... }: {
+  home-manager.users.${user} = { config, pkgs, ... }: {
     programs.bash.enable = true;
 
-    home.username = "jiucheng";
-    home.homeDirectory = "/home/jiucheng";
+    home.username = "${user}";
+    home.homeDirectory = "/home/${user}";
 
     # This value determines the Home Manager release that your configuration is
     # compatible with. This helps avoid breakage when a new Home Manager release
@@ -37,11 +38,11 @@ in
     home.stateVersion = "23.11"; # Please read the comment before changing.
 
     imports = [
-      ./${share_folder}/jiucheng/modules/git.nix
       ./modules/zsh.nix
       ./modules/i3.nix
       ./modules/vscode.nix
       ./x86-addon.nix
+      ./${share_folder}/${user}/modules/git.nix
     ];
 
     # Allow unfree Software
@@ -86,19 +87,19 @@ in
       # Latex Setup
       (pkgs.texlive.combine {
         inherit (pkgs.texlive)
-          scheme-medium
-          enumitem
-          subfigure
-          calrsfs
-          changepage
-          framed
-          subfiles
-          pgfplots
-          fontsize
-          xecjk
-          ;
+        scheme-medium
+        enumitem
+        subfigure
+        calrsfs
+        changepage
+        framed
+        subfiles
+        pgfplots
+        fontsize
+        xecjk
+        ;
       })
- 
+
     ];
 
     # Home Manager is pretty good at managing dotfiles. The primary way to manage
@@ -114,18 +115,18 @@ in
       #   org.gradle.console=verbose
       #   org.gradle.daemon.idletimeout=36§00000
       # '';
-      
-      ".config/htop/htoprc".source = ./${share_folder}/jiucheng/dotfiles/htoprc.config;
+
+      ".config/htop/htoprc".source = ./${share_folder}/${user}/dotfiles/htoprc.config;
       ".config/qterminal.org/qterminal.ini".source = dotfiles/qterminal.config;
       ".config/i3blocks/config".source = dotfiles/i3blocks.config;
       # ".background-image".source = ./${share_folder}/background-image/LandScaping/pexels-8moments.jpg;
       ".background-image".source = ./${share_folder}/background-image/cityGirl.jpg;
       # ".background-image".source = ./${share_folder}/background-image/littleBoy.png;
-      ".face".source = ./${share_folder}/jiucheng/face.png;
-      ".ssh/config".source = ./${share_folder}/jiucheng/dotfiles/ssh_config.config;
-      ".config/gitui/key_bindings.ron".source = ./${share_folder}/jiucheng/dotfiles/gitui_key_bindings.ron;
+      ".face".source = ./${share_folder}/${user}/face.png;
+      ".ssh/config".source = ./${share_folder}/${user}/dotfiles/ssh_config.config;
+      ".config/gitui/key_bindings.ron".source = ./${share_folder}/${user}/dotfiles/gitui_key_bindings.ron;
 
-      
+
       "bin" = {
         source = ./script;
         recursive = true;

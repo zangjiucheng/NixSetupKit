f62bd9131ab63b008be8a5825392a563a6ef0249
Jiucheng Zang <git.jiucheng@gmail.com>
2025-02-09 14:31:16 -0500

    343 current  2025-02-09 14:31:10  x86-user-24.11.20250206.f5a32fa  6.6.75                          *

--- modules/shared/configuration.nix
+++ modules/shared/configuration.nix
@@ -6,7 +6,6 @@
 {
   imports =
     [
-      ../../hardware-configuration.nix
       ./default.nix
     ];
 
--- nix-config/user/user-nixos/jiucheng/script/remoteWindows.sh
+++
@@ -1,3 +0,0 @@
-#! /usr/bin/sh
-
-xfreerdp /f /u:j7zang /p:23010320050111zjC! /v:windows.student.math.uwaterloo.ca /sound
